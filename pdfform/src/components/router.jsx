38e049d81b3f3fcc384a3da57204ffd8984974fe
Manoj Kumar Padhy <manojkumarpadhyms@gmail.com>
2024-01-09 21:02:57 +0530

    sign in page created

--- pdfform/src/components/router.jsx
+++ pdfform/src/components/router.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Signup } from "../pages/signup";
 import { Login } from "../pages/login";
+import { Profile } from "../pages/profile";
 
 export const Router=()=>{
     return (
         <Routes>
            <Route path="/signup" element={<Signup/> }/>
            <Route path="/login" element={<Login/> }/>
-
+           <Route path="/createprofile" element={<Profile/> }/>
         </Routes>
     )
 }
--- pdfform/src/pages/login.jsx
+++ pdfform/src/pages/login.jsx
@@ -1,10 +1,93 @@
-import { Box } from "@chakra-ui/react";
-import React from "react";
+import {
+    Box, Button, chakra, Flex, FormControl,
+    FormLabel, Heading, Input, InputGroup,
+    InputRightElement, Stack, Text
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
+import { Link, useNavigate } from "react-router-dom";
 
-export const Login=()=>{
+export const Login = () => {
+
+    const [showPassword, setShowPassword] = useState(false)
+    const [user, setUser] = useState({
+        Email: null,
+        Password: null
+    })
+    const navigate = useNavigate()
+    // function for sign in
+    const handelSignin=()=>{
+        console.log(user)
+        navigate('/createprofile')
+    }
+
     return (
         <Box>
-            
+            <Heading textAlign={'center'} mb={10}>Login to your Profile</Heading>
+            <Box
+                rounded={'lg'}
+                bg={'white'}
+                boxShadow={'lg'}
+                p={8}
+                maxW={'500px'}
+                m={"auto"}
+            >
+                <form
+               onSubmit={(e)=>{
+                e.preventDefault()
+                handelSignin()
+               }} 
+                >
+                    <Heading size={'md'} textAlign={'center'} mb={4}>Sign In</Heading>
+                    <FormControl isRequired>
+                        <FormLabel>Email Address</FormLabel>
+                        <Input
+                            onChange={(e) => {
+                                setUser({
+                                    ...user,
+                                    Email:e.target.value
+                                })
+                            }}
+                            placeholder='Email' type="email" />
+                    </FormControl>
+                    <FormControl mt={4} isRequired>
+                        <FormLabel>Password</FormLabel>
+                        <InputGroup>
+                            <Input
+                                onChange={(e) => {
+                                    setUser({
+                                        ...user,
+                                        Password:e.target.value
+                                    })
+                                }}
+                                placeholder="Password"
+                                type={showPassword ? 'text' : 'password'} />
+                            <InputRightElement h={'full'}>
+                                <Button
+                                    variant={'ghost'}
+                                    onClick={() => setShowPassword((showPassword) => !showPassword)}>
+                                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                                </Button>
+                            </InputRightElement>
+                        </InputGroup>
+                    </FormControl>
+                    <Input type="submit"
+                        mt={8}
+                        value={'Sign In'}
+                        cursor={"pointer"}
+                        bg={'blue'} color={'white'} />
+                </form>
+                <Stack pt={6}>
+                    <Text align={'center'}>
+                        Don't have account? <chakra.span color={'blue.400'}
+                            onClick={() => {
+                                navigate('/signup')
+                            }}
+                        ><Link >Sign Up</Link> </chakra.span>
+                    </Text>
+                </Stack>
+            </Box>
+
         </Box>
     )
 }
